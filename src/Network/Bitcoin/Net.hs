d2201a3db47e4d20e310866efe7f656827784291
Eric Pashman <eric.pashman@gmail.com>
2013-05-06 13:55:12 -0500

    Fix parsing of data about connected peers.
    Bad typing caused calls to getPeerInfo to throw exceptions. For parsing
    to succeed, the peerSubversion field of PeerInfo must have type Text and
    the result type of getPeerInfo must contain a list.
    
    After updates, PeerInfo remains incompletely documented.

--- src/Network/Bitcoin/Net.hs
+++ src/Network/Bitcoin/Net.hs
@@ -21,17 +21,16 @@ import Network.Bitcoin.Internal
 getConnectionCount :: Auth -> IO Integer
 getConnectionCount auth = callApi auth "getconnectioncount" []
 
--- | Information on a given connected node in the network.
+-- | Information about a peer node of the Bitcoin network.
 --
 --   The documentation for this data structure is incomplete, as I honestly
 --   don't know what some of these fields are for. Patches are welcome!
 data PeerInfo =
-    PeerInfo { -- | The ip:port of this peer, as a string.
+    PeerInfo { -- | The IP:port of this peer, as a string.
                addressName :: Text
              , services :: Text
-             -- | Relative to when we first time we conected with this peer
-             --   (and in milliseconds), the last time we sent this peer any
-             --   data.
+             -- | Relative to the first time we conected with this peer (and in
+             -- milliseconds), the last time we sent this peer any data.
              , lastSend :: Integer
              -- | Relative to the first time we connected with this peer
              --   (and in milliseconds), the last time we sent this peer any
@@ -40,10 +39,10 @@ data PeerInfo =
               -- | How long have we been connected to this peer (in
               --   milliseconds).
              , connectionTime :: Integer
-             -- | The version of bitcoind the peer is running.
+             -- | The version of the Bitcion client the peer is running.
              , peerVersion :: Integer
-             -- | The sub-version of bitcoind the peer is running.
-             , peerSubversion :: Integer
+             -- | The sub-version of the Bitcoin client the peer is running.
+             , peerSubversion :: Text
              , inbound :: Bool
              , releaseTime :: Integer
              , startingHeight :: Integer
@@ -66,6 +65,6 @@ instance FromJSON PeerInfo where
                                     <*> o .: "banscore"
     parseJSON _ = mzero
 
--- | Returns data about each connected network node.
-getPeerInfo :: Auth -> IO PeerInfo
+-- | Returns data about all connected peer nodes.
+getPeerInfo :: Auth -> IO [PeerInfo]
 getPeerInfo auth = callApi auth "getpeerinfo" []
