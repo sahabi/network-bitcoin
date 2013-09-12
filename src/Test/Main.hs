4aa979852382ebcff48695bcd158aa3d67c46191
Schell Scivally <efsubenovex@gmail.com>
2013-09-12 11:23:29 -0700

    Added IO Test stub.

--- src/Test/Main.hs
+++ src/Test/Main.hs
@@ -1,20 +1,33 @@
 module Main where
 
-import Test.Framework ( Test, defaultMain, testGroup )
+import Test.Framework ( defaultMain )
 import Test.Framework.Providers.QuickCheck2
+import Test.Framework.Providers.API
 import Test.QuickCheck
 
 main :: IO ()
-main = defaultMain tests 
+main = defaultMain $ tests ++ testsIO
 
 
 tests :: [Test]
 tests = [ testGroup "Test test" [testTest] ]
 
 
+testsIO :: [Test]
+testsIO = map buildTest [ ioTest ]
+
+
 testTest :: Test
-testTest = testProperty "test test" prop_test
+testTest = testProperty "Test" prop_test
+
+
+ioTest :: IO Test
+ioTest = do
+    putStrLn "Making an IO test."
+    return $ testProperty "IO Test" prop_test 
 
 
 prop_test :: Bool -> Bool
 prop_test b = b || not b
+
+
