6708880f69d63f86eea34668189011c915f33a79
Schell Scivally <efsubenovex@gmail.com>
2013-09-12 11:10:34 -0700

    Added stub test.

--- src/Test/Main.hs
+++ src/Test/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+import Test.Framework ( Test, defaultMain, testGroup )
+import Test.Framework.Providers.QuickCheck2
+import Test.QuickCheck
+
 main :: IO ()
-main = putStrLn "network-bitcoin-test"
+main = defaultMain tests 
+
+
+tests :: [Test]
+tests = [ testGroup "Test test" [testTest] ]
+
+
+testTest :: Test
+testTest = testProperty "test test" prop_test
+
+
+prop_test :: Bool -> Bool
+prop_test b = b || not b
